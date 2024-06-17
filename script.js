ce0e6959fc4712af390c81ed0915b58686aa0b4a
Martin Gausseran <martin.gausseran@gmail.com>
2024-06-17 11:38:28 +0200

    Ajout d'une deuxième liste "phrases"

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 let listeMots = ["balançoire", "zigouigoui", "anticonstitutionnellement", "poubelle", "atome", "train", "voiture", "zouave", "évaluation"];
-
+let listePhrases = ["Pas de panique !", "La vie, l’univers et le reste", "Merci pour le poisson"];
 let score = 0;
-let randomIndex = Math.floor(Math.random() * listeMots.length)
-let motUtilisateur = prompt("Ecrivez le mot suivant : " + listeMots[randomIndex]);
+let randomIndexMots = Math.floor(Math.random() * listeMots.length);
+let randomIndexPhrases = Math.floor(Math.random() * listePhrases.length);
 
-if (motUtilisateur === listeMots[randomIndex]) {
-    console.log("Bravo, vous avez bien orthographié le mot !")
-    score += 1;
-    alert(`Vous avez ${score} points !`)
+let choix = prompt("Veuillez choisir entre 'mots' ou 'phrases': ");
+while (choix !== "mots" && choix !== "phrases") {
+    prompt("Veuillez choisir l'une des deux listes proposées.")
+}
+if (choix === "mots") {
+    let motUtilisateur = prompt("Ecrivez le mot suivant : " + listeMots[randomIndexMots]);
+    if (motUtilisateur === listeMots[randomIndexMots]) {
+        console.log("Bravo, vous avez bien orthographié le mot !")
+        score += 1;
+        alert(`Vous avez ${score} point(s) !`);
+    }
+    else {
+        alert("Vous avez fait une erreur de frappe.");
+        alert(`Vous avez ${score} point(s)!`);
+    }
+
 }
 else {
-    alert("Vous avez fait une erreur de frappe.")
-}
+    let phraseUtilisateur = prompt("Ecrivez la phrase suivante: " + listePhrases[randomIndexPhrases]);
+    if (motUtilisateur === listeMots[randomIndexPhrases]) {
+        console.log("Bravo, vous avez bien orthographié la phrase !")
+        score += 1;
+        alert(`Vous avez ${score} point(s) !`);
+    }
+    else {
+        alert("Vous avez fait une erreur de frappe.");
+        alert(`Vous avez ${score} point(s)!`);
+    }
+};
